610d82e127f7b1d85f95ed2e34bf7bc218395724
alvinary <alvaro.cuestas.ll@gmail.com>
2023-11-15 20:40:37 -0300

    Add draft proof of par commutativity

--- Concurrent_Kleene_Algebra.thy
+++ Concurrent_Kleene_Algebra.thy
@@ -47,7 +47,7 @@ proof
   show
     " \<And>a b c. (a @@ b) @@ c = a @@ b @@ c "
     unfolding conc_def
-    by blast
+    by simp_all blast
   show
     " \<And>a. {[]} @@ a = a "
     by auto
@@ -155,13 +155,13 @@ locale concurrent_kleene_algebra =
     one :: "'a" ("1") and
     plus :: "'a \<Rightarrow> 'a \<Rightarrow> 'a" and
     seq :: "'a \<Rightarrow> 'a \<Rightarrow> 'a" (infixl ";" 70) and
-    par :: "'a \<Rightarrow> 'a \<Rightarrow> 'a" (infixl "|" 60) and
+    par :: "'a \<Rightarrow> 'a \<Rightarrow> 'a" (infixl "||" 60) and
     zero :: "'a" ("0") and
     leq :: "'a \<Rightarrow> 'a \<Rightarrow> bool" (infixl "\<sqsubseteq>" 40) and
     sup :: "'a set \<Rightarrow> 'a" +
   assumes
     exchange_law :
-      " a | b ; c | d \<sqsubseteq> a ; c | b ; d "
+      " (a || b) ; (c || d) \<sqsubseteq> (b ; c) || (a ; d) "
 
 interpretation union_concatenation_quantale :
   quantale "{[]}" "(\<union>)" "(@@)" "empty" "(\<subseteq>)" "\<Union>"
@@ -176,10 +176,10 @@ qed
 interpretation union_shuffle_quantale :
   quantale "{[]}" "(\<union>)" "(\<parallel>)" "empty" "(\<subseteq>)" "\<Union>"
 proof
-  show " \<And>a A. a \<parallel> \<Union> A =  \<Union> {a \<parallel> x |x. x \<in> A} "
+  show " \<And> a A. a \<parallel> \<Union> A =  \<Union> {a \<parallel> x |x. x \<in> A} "
     unfolding Shuffle_def
     by auto
-  show "  \<And>x A. (\<And>y. y \<in> A \<Longrightarrow> y \<subseteq> x) \<Longrightarrow> \<Union> A \<subseteq> x "
+  show "  \<And> x A. (\<And>y. y \<in> A \<Longrightarrow> y \<subseteq> x) \<Longrightarrow> \<Union> A \<subseteq> x "
     unfolding Shuffle_def
     by auto
 qed
@@ -187,15 +187,132 @@ qed
 interpretation shuffle_languages_cka :
   concurrent_kleene_algebra "{[]}" "(\<union>)" "(@@)" "(\<parallel>)" "empty" "(\<subseteq>)" "\<Union>"
 proof
-  show "  \<And>a b c d. (a \<parallel> (b @@ c)) \<parallel> d \<subseteq> (a @@ c) \<parallel> (b @@ d) "
+  show "  \<And>a b c d. a \<parallel> b @@ c \<parallel> d \<subseteq> (b @@ c) \<parallel> (a @@ d) "
     unfolding Shuffle_def conc_def
     by simp_all blast
 qed
 
-(*
-lemma (in concurrent_kleene_algebra)
-  par_contains_seq : " a ; b \<sqsubseteq> a | b "
-  sorry 
-*)
+context concurrent_kleene_algebra
+begin
+lemma one_is_seq_neuter_right [simp] :
+  " x ; 1 = x "
+  by auto
+
+lemma one_is_seq_neuter_left [simp] :
+  " 1 ; x = x "
+  by auto
+
+lemma one_is_par_neuter_right [simp] :
+  " x || 1 = x "
+  by auto
+
+lemma one_is_par_neuter_left [simp] :
+  " 1 || x = x "
+  by auto
+
+lemma single_par_exchange :
+  " (x || y) ; z \<sqsubseteq> x || (b;c) "
+  sorry
+
+lemma par_is_commutative [simp] :
+  " x || y = y || x "
+proof -
+  have exchange : " (x || y) ; (z || w) \<sqsubseteq> (y ; z) || (x ; w) " by (metis exchange_law)
+  assume z_is_one : "z = 1"
+  assume w_is_one : "w = 1"
+  have replace_by_one :
+    " (x || y) ; (1 || 1) \<sqsubseteq> (y ; 1) || (x ; 1) "
+    by (metis exchange z_is_one w_is_one)
+  have simplify_ones :
+    " (x || y ) \<sqsubseteq> (y || x) "
+    by (metis
+          replace_by_one
+          one_is_seq_neuter_right
+          one_is_par_neuter_right)
+  have swapped_exchange :
+    " (y || x) ; (z || w) \<sqsubseteq> (x ; z) || (y ; w) "
+    by (metis exchange_law)
+  have swapped_replace_by_one : 
+    " (y || x) ; (1 || 1) \<sqsubseteq> (x ; 1) || (y ; 1) "
+    by (metis swapped_exchange z_is_one w_is_one)
+  have swapped_simplify_ones :
+    " (y || x ) \<sqsubseteq> (x || y) "
+    by (metis
+          swapped_replace_by_one
+          one_is_seq_neuter_right
+          one_is_par_neuter_right)
+  have symmetric_order :
+    "(y || x ) \<sqsubseteq> (x || y) \<and> (x || y ) \<sqsubseteq> (y || x) "
+    by (metis
+          swapped_simplify_ones
+          simplify_ones)
+  have commutativity :
+    " (y || x) = (x || y) "
+    sorry
+    (* by (metis
+          symmetric_order
+          leq_is_antisymmetric) *)
+qed
+end
 
 end
+
+(*
+lemma par_is_commutative [simp] :
+  " x || y = y || x "
+  
+  {aa} || {bb, dd} = {bb, dd} || {aa} =
+  {
+   aabb,
+   abab,
+   abba,
+   baba,
+   baab,
+   bbaa,
+   aadd,
+   adad,
+   adda,
+   dada,
+   daad,
+   ddaa,
+  }
+  
+  sanity check: 
+  4! / (2!)(2!) = 4 3 2 1 / 4 = 3 2 = 6
+  
+  (x | y) ; (z | w) <= (x ; z) | (y ; w) =
+  	{ (w/1, x/1) }
+  (1 | y) ; (z | 1) <= (1 ; z) | (y ; 1) =
+  	{  }
+  (1 | y) ; (z | 1) <= (1 ; z) | (y ; 1) =
+  	
+*)
+
+(*
+lemma par_contains_seq :
+  " x ; w \<sqsubseteq> x | w "
+proof -
+  assume exchange : " (x | y) ; (z | w) \<sqsubseteq> (x ; z) | (y ; w) "
+  assume y_is_one : " y = 1 "
+  assume z_is_one : " z = 1 "
+    have
+    replace_by_one :
+    " (x | 1) ; (1 | w) \<sqsubseteq> (x ; 1) | (1 ; w) "
+    by (metis
+        exchange
+        z_is_one
+        y_is_one)
+  have
+    simplify_pars :
+    " x ; w  \<sqsubseteq> (x ; 1) | (1 ; w) "
+    by (metis
+        replace_by_one
+        one_is_par_neuter_right
+        one_is_par_neuter_left)
+  have
+    simplify_seqs :
+    " x ; w  \<sqsubseteq> x | w "
+    by (metis simplify_pars
+        one_is_seq_neuter_right
+        one_is_seq_neuter_left)
+*)
